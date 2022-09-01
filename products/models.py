20b0bd814831d4ec26708f0fd32c7d2847c1a8a4
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-09-01 17:35:36 +0000

    add another field to product model and migrate

--- products/models.py
+++ products/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    year = models.DecimalField(max_digits=4, decimal_places=4, null=True)
 
     def __str__(self):
         return self.name
