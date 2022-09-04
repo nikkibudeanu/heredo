501b99aacebc4bbf0629a4ecd91f811960a5b7c3
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-09-04 14:40:03 +0000

    add function so the bag counts the product total

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,25 +1,38 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
     bag_items = []
-    total = 0
-    product_count = 0
+    total_sum = 0
+    product_number = 0
+    bag = request.session.get('bag', {})
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total_sum += quantity * product.price
+        product_number += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    if total_sum < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total_sum * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
     
-    grand_total = delivery + total
+    grand_total = delivery + total_sum
     
     context = {
         'bag_items': bag_items,
-        'total': total,
-        'product_count': product_count,
+        'total_sum': total_sum,
+        'product_number': product_number,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
