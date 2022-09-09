9ff9965ec09c3595df0a7f9b019a3d9e44c34567
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-09-09 18:34:00 +0000

    change jquery link

--- bag/views.py
+++ bag/views.py
@@ -47,17 +47,16 @@ def update_bag(request, item_id):
 
 def remove_item_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
-
     try:
         product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
-        
+
         bag.pop(item_id)
         messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
-
+        
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
