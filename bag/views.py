12f862906adccf24ee0b7ff79b372d659b36dab3
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-09-11 13:18:24 +0000

    change toasts background color

--- bag/views.py
+++ bag/views.py
@@ -12,7 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a product to the shopping bag """
     
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
