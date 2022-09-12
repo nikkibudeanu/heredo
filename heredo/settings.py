e18a27b680c2db4312e1b0eb5f581735add81f59
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-09-12 18:20:32 +0000

    Add crispy forms and add checkout html content

--- heredo/settings.py
+++ heredo/settings.py
@@ -66,6 +66,10 @@ TEMPLATES = [
                 'bag.contexts.bag_contents',
 
             ],
+             'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
