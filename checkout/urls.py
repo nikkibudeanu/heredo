fd2f310645b8b2b5740338fc034c2a7319ba87c6
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-09-15 14:46:58 +0000

    Correct typo error to solve bug

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('payment_success/<order_number>', views.payment_succes, name='payment_success'),
+    path('payment_success/<order_number>', views.payment_success, name='payment_success'),
 ]
