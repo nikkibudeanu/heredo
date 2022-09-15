33c422a2e18d33c0b6a5e3c8d3919ef40a43b82a
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-09-15 05:09:30 +0000

    add stripe js links and create folders and files

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LYBaOEqAJbeRep0mf1bdGQvOvm0XYUeT32IRVBgDR8aDISffJaYN8Wnq7rsATTwvba2NMItSOZuhfUKAugNSb5Z00ytpMiMYe',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
