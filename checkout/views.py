739ae6daf47e4d3ddfe51730f4bdbe5822066ccd
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-10-02 14:11:17 +0000

    add user information to payment success view

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from bag.contexts import bag_contents
 
 import stripe
@@ -122,6 +124,28 @@ def payment_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    profile = UserProfile.objects.get(user=request.user)
+    # Link user profile to the order
+    order.user_profile = profile
+    order.save()
+
+    # Save the user information
+    if save_info:
+        profile_data = {
+            'default_phone_number': order.phone_number,
+            'default_country': order.country,
+            'default_postcode': order.postcode,
+            'default_town_or_city': order.town_or_city,
+            'default_street_address1': order.street_address1,
+            'default_street_address2': order.street_address2,
+            'default_county': order.county,
+        }
+        user_profile_form = UserProfileForm(profile_data, instance=profile)
+        if user_profile_form.is_valid():
+            user_profile_form.save()
+
+
     messages.success(request, f'Hooray! Order completed succesfully! \
         Your order number is :  {order_number}. Check your email:  {order.email} for a confirmation.')
 
