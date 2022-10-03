d14830e6d134e5a6cbd880a3584055be518be235
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-10-03 10:05:34 +0000

    add url and views to edit a product as an admin

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.all_products, name="products"),
     path('<int:product_id>/', views.product_detail, name="product_detail"),
     path('add/', views.admin_add_product, name='admin_add_product'),
+    path('edit/<int:product_id>/', views.admin_edit_product, name='admin_edit_product'),
 ] 
--- products/views.py
+++ products/views.py
@@ -77,3 +77,26 @@ def admin_add_product(request):
     }
 
     return render(request, template, context)
+
+def admin_edit_product(request, product_id):
+    """ Update a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Success! Your product is updated!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Oops! Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are updating {product.name}')
+
+    template = 'products/admin_edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
