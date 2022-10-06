8b8e6bdb1acc966192a5e3723a729903105d415e
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-10-06 14:30:25 +0000

    add cache control parameters to the settings file

--- heredo/settings.py
+++ heredo/settings.py
@@ -1,5 +1,3 @@
-
-from pathlib import Path
 import os
 import dj_database_url
 
@@ -9,7 +7,7 @@ if os.path.isfile("env.py"):
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -181,6 +179,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'heredo'
     AWS_S3_REGION_NAME = 'eu-west-1'
