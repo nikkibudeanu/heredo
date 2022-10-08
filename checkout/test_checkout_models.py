33b0e9648fe3429c4afe2ce8a78c181dbbf4a71d
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-10-08 18:47:41 +0000

    add test for the order number model

---
+++ checkout/test_checkout_models.py
@@ -0,0 +1,38 @@
+"""Test Checkout Models"""
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Order 
+
+
+class TestCheckoutModels(TestCase):
+    """ Class to test the checkout Models """
+
+    def setUp(self):
+        """
+        Set up test data
+        """
+        testuser = User.objects.create_user(
+            username='test_user',
+            password='test_password',
+            email='test@test.com')
+        testuser.save()
+
+        Order.objects.create(
+            order_number='987654321',
+            full_name='Test User',
+            email='test@test.com',
+            phone_number='1234567890',
+            country='Test Country',
+            postcode='Test postcode',
+            town_or_city='Test city',
+            street_address1='Test address',
+            county='Test country',
+        )
+
+
+    def test_order_number(self):
+        """
+        Test the order number
+        """
+        order = Order.objects.get(email='test@test.com')
+        self.assertEqual(str(order), order.order_number)
