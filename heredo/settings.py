aca15e5f4b9449ebbda2531b7c883aafdc067a53
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-10-11 09:09:39 +0000

    add context processors for media in the settings file

--- heredo/settings.py
+++ heredo/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
+                'django.template.context_processors.media',
 
             ],
              'builtins': [
